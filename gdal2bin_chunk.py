306dc1b46fb198950f548fc30ddf5478fbfb760e
albhasan <albhasan@gmail.com>
2017-05-05 15:29:05 -0300

    getGdalMetadata bug fixed

--- gdal2bin_chunk.py
+++ gdal2bin_chunk.py
@@ -5,104 +5,155 @@ import numpy
 from array import array
 from gdal2bin_util import *
 
-
-
-#********************************************************
+################################################################################
 # MAIN
-#********************************************************
+#
+# sdb2bin(files, chunk, chunkTranslation)
+#
+#-------------------------------------------------------------------------------
+#
+# find /home/scidb/LANDSAT/landsat8Original/SurfaceReflectance/2013 -name 'LC822606*tif'
+# find /home/scidb/MODIS -type f -name '*h12v10*'
+#
+# python gdal2bin_chunk.py "/home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_solar_zenith_band4.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_solar_azimuth_band4.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_radsat_qa.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_sr_band1.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_pixel_qa.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_sr_band4.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_sr_band7.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_sr_band3.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_sr_band5.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_sr_band2.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_sr_cloud_qa.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_sr_atmos_opacity.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_sensor_zenith_band4.tif /home/alber/landsat/collection1/LE072310652016012501T1-SC20170407161252/LE07_L1TP_231065_20160125_20161015_01_T1_sensor_azimuth_band4.tif" 75 75 75 75
+#
+# Ubuntu uses an old version of numpy
+# sudo easy_install --upgrade numpy
+# sudo easy_install --upgrade scipy
+################################################################################
 def main(argv):
-	parser = argparse.ArgumentParser(description = "Exports GDAL images to the stdout using SciDB's binary format.")
-	parser.add_argument("inputFiles", help = "List of images separated by spaces.")
-	parser.add_argument("col", help = "Number of the column from where to start getting data.")
-	parser.add_argument("row", help = "Number of the row from where to start getting data.")
-	parser.add_argument("colbuf", help = "Number of additional columns to get data from.")
-	parser.add_argument("rowbuf", help = "Number of additional rows to get data from.")
-	parser.add_argument("--log", help = "Log level. Default = WARNING", default = 'WARNING')
-	#Get paramters
-	args = parser.parse_args()
-	inputFiles = args.inputFiles
-	col = int(args.col)
-	row = int(args.row)
-	colbuf = int(args.colbuf)
-	rowbuf = int(args.rowbuf)
-	log = args.log
-	####################################################
-	# CONFIG
-	####################################################
-	gdal.UseExceptions()														# use GDAL's error messages #gdal.DontUseExceptions()
-	# log
-	numeric_loglevel = getattr(logging, log.upper(), None)
-	if not isinstance(numeric_loglevel, int):
-		raise ValueError('Invalid log level: %s' % log)
-	logging.basicConfig(filename = 'log_gdal2bin_chunk.log', level = numeric_loglevel, format = '%(asctime)s %(levelname)s: %(message)s')
-	logging.info("gdal2bin_chunk: " + str(args))
-	####################################################
-	# SCRIPT
-	####################################################
-	flist = inputFiles.split()													# get the list of files
-
-	# sort by image-band
-	imgfiles = {}
-	for i in range(0, len(flist)):
-		imgfiles[completeBandNumber(os.path.basename(flist[i]))] = flist[i]
-
-	imgfiles = collections.OrderedDict(sorted(imgfiles.items()))
-
-	# get image names and files metadata
-	imgset = set()
-	imgfilesmd = list()
-	for key in imgfiles:
-		filemd = getFileNameMetadata(imgfiles[key])								# get metadata from the file name
-		imgfilesmd.append(filemd)												
-		imgset.add(filemd['image'])												# get images' names
-
-	# a file per band or file per image?
-	fileperband = True
-	if len(imgset) == len(imgfiles):
-		fileperband = False
-
-	# get pixels from each file ordered by band
-	imgpixlist = []
-	bandtypes = []																# GDAL band types of an images
-	if fileperband:
-		for img in imgset:
-			ifiles = []															# a list of paths to bands of the same image
-			for md in imgfilesmd:
-				if img == md['image']:
-					ifiles.append(md['filepath'])
-			imgpixlist.append(getPixelImages(ifiles, col, row, colbuf, rowbuf, 0)) 	# get the pixels of all the bands (files) of a single image
-		# get band's datatypes using the bands of the last image
-		for i in ifiles:
-			bandtypes.append(getImageMetadata(i)['bandtype'])
-	else:
-		for ifile in imgfiles:
-			imgpixlist.append(getPixels(ifile, x, y, xchunk, ychunk, 0))
-		bandtypes = getImageMetadata(ifile)['bandtype']
-
-	# unlist band types
-	if isinstance(bandtypes[0], list):	
-		bandtypes = sum(bandtypes, [])
-
-	# cast to array
-	# re-arrange dimensions from:	band_id, col_id, row_id, time_id
-	# to:							col_id, row_id, time_id, band_id
-	pixarrays = numpy.transpose(numpy.stack(imgpixlist, axis = 1))
-
-	# write the binary
-	bandtyp, bandpos = findrep(bandtypes)										# lists of consequetive band types and their positions
-	for cid in range(pixarrays.shape[0]):
-		for rid in range(pixarrays.shape[1]):
-			for tid in range(pixarrays.shape[2]):
-				idxa = array('L',[cid, rid, tid]) 								# sdb's array dimensions - L unsigned long
-				idxa.tofile(sys.stdout)
-				s = 0
-				for i in range(len(bandtyp)):
-					valsa = array(mapGdal2python(bandtyp[i]), pixarrays[cid, rid, tid][s:bandpos[i]])
-					valsa.tofile(sys.stdout)
-					s = bandpos[i] + 1
+    parser = argparse.ArgumentParser(description = "Export GDAL images to the stdout using SciDB's binary format.")
+    parser.add_argument("inputFiles", help = "List of images separated by spaces.")
+    parser.add_argument("col", help = "Number of the column from where to start getting data.")
+    parser.add_argument("row", help = "Number of the row from where to start getting data.")
+    parser.add_argument("colbuf", help = "Number of additional columns to get data from.")
+    parser.add_argument("rowbuf", help = "Number of additional rows to get data from.")
+    parser.add_argument("coltrans", help = "Translation applied to the column index.")
+    parser.add_argument("rowtrans", help = "Translation applied to the row index.")
+    parser.add_argument("--log", help = "Log level. Default = WARNING", default = 'WARNING')
+    #Get paramters
+    args = parser.parse_args()
+    inputFiles = args.inputFiles
+    col = int(args.col)
+    row = int(args.row)
+    colbuf = int(args.colbuf)
+    rowbuf = int(args.rowbuf)
+    coltrans = int(args.coltrans)
+    rowtrans = int(args.rowtrans)
+    log = args.log
+    ####################################################
+    # CONFIG
+    ####################################################
+    gdal.UseExceptions()                                                        # use GDAL's error messages #gdal.DontUseExceptions()
+    # log
+    numeric_loglevel = getattr(logging, log.upper(), None)
+    if not isinstance(numeric_loglevel, int):
+        raise ValueError('Invalid log level: %s' % log)
+    logging.basicConfig(filename = 'log_gdal2bin_chunk.log', level = numeric_loglevel, format = '%(asctime)s %(levelname)s: %(message)s')
+    logging.info("gdal2bin_chunk: " + str(args))
+    ####################################################
+    # SCRIPT
+    #---------------------------------------------------
+    # [1] image == [n] bands of the same path/row and date
+    # [1] image series == [n] images of the same satellite, sensor, path and row but different acquisition time
+    # [1] band  == [1]file
+    # [1] file  == [n] bands
+    ####################################################
+    # sort files by image-path/row-band
+    files = sortFiles(inputFiles.split())
+    # get files' metadata
+    imgseries = set()                                                           # set of series of images
+    filesmd = list()                                                            # list of metadata derived from file names
+    for key,value in files.items():
+        fmd = getFileNameMetadata(value)
+        filesmd.append(fmd)
+        imgseries.add(fmd['satellite'] + fmd['sensor']+ fmd['path'] + fmd['row'])
+    # validation
+    if len(imgseries) != 1:
+        logging.error("Invalid number of time series of images: " + imgseries)
+        sys.exit(0)
+    # build list of images and filepaths
+    imgfiles = imgseries2imgfp(filesmd)                                         # List [img, [filepaths]]
+    # validation
+    if len(imgfiles) == 0:
+        logging.error("No images found")
+        sys.exit(0)
+    nimg = len(imgfiles[0][1])
+    for i in range(1, len(imgfiles)):
+        if nimg != len(imgfiles[i][1]):
+            logging.error("Inconsistent number of files in images: " + imgfiles[i][0])
+            sys.exit(0)
+    # get band's datatypes from of a single image
+    bandtypes = []                                                              # GDAL band types of an image's bands
+    for fp in imgfiles[0][1]:
+        bandtypes.append(getGdalMetadata(fp)['bandtype'])
+    bandtypes = sum(bandtypes, []) if isinstance(bandtypes[0], list) else bandtypes
+    # get time_id parameters
+    tidparam = gettimeidparameters(filesmd[0]['satellite'])
+    # get pixels from each image
+    for ifiles in imgfiles:
+        tid = ymd2tid(ifiles[0][-8:], tidparam['origin'], tidparam['period'], tidparam['yearly'])
+        imgpixs = getPixelImages(ifiles[1], col, row, colbuf, rowbuf, -1)       # pixels of the bands of an image. A numpy.ndarray object
+        for i in range(colbuf):
+            rid = i + rowtrans
+            for j in range(rowbuf):
+                cid = j + coltrans
+                pixval = imgpixs[i, j]
 
 
-	
+
+
+                idxa = array('L',[cid, rid, tid])                                 # sdb's array dimensions - L unsigned long
+                idxa.tofile(sys.stdout)
+
+
+
+
+
+
+    # TODO: write the numpy array
+
+
+
+
+
+
+
+        
+
+
+
+            
+
+    
+    
+    
+    
+    
+################################################################################
+
+
+    pixarrays = numpy.transpose(numpy.stack(imgpixlist, axis = -1))         # cast list to array
+    #pixarray = numpy.stack(imgpixlist, axis = 0)
+
+
+
+
+    # write
+    bandtyp, bandpos = findrep(bandtypes)                                        # lists of consequetive band types and their positions
+    for cid in range(pixarrays.shape[0]):
+        for rid in range(pixarrays.shape[1]):
+            for tid in range(pixarrays.shape[2]):
+                idxa = array('L',[cid, rid, tid])                                 # sdb's array dimensions - L unsigned long
+                idxa.tofile(sys.stdout)
+                s = 0
+                for i in range(len(bandtyp)):
+                    valsa = array(mapGdal2python(bandtyp[i]), pixarrays[cid, rid, tid][s:bandpos[i]])
+                    valsa.tofile(sys.stdout)
+                    s = bandpos[i] + 1
+
+
+    
 if __name__ == "__main__":
    main(sys.argv[1:])
 
