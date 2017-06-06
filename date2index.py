b2727d5931edeff1e50b2be9315f39b88ee8b2a9
albhasan <albhasan@gmail.com>
2017-06-06 19:14:49 -0300

    added support for YYYDOY dates

--- date2index.py
+++ date2index.py
@@ -2,21 +2,27 @@
 import sys
 import argparse
 from gdal2bin_util import ymd2tid
+from gdal2bin_util import ydoy2ymd
 
 def main(argv):
     parser = argparse.ArgumentParser(description = "Compute a SciDB time index from a date.")
-    parser.add_argument("ymd", help = "A date represented by numbers like this: YYYYMMDD")
+    parser.add_argument("ymd", help = "A date represented by numbers like YYYYMMDD or YYYYDOY")
     parser.add_argument("origin", help = "An date YYYYMMDD. The day when the time_id is 0")
     parser.add_argument("period", help = "The number of days between two consequtive time_id")
     parser.add_argument("--yearly", help = "Do the dates yearly match January the 1st? (Default = True)", default = 'True')
     # get parameters
     args = parser.parse_args()
-    ymd = int(args.ymd)
-    origin = int(args.origin)
-    period = int(args.period)
+    ymd = args.ymd
+    origin = args.origin
+    period = args.period
     yearly = args.yearly in ['True', 'true', 'T', 't', 'YES', 'yes', 'Y', 'y']
     # script
-    print(str(ymd2tid(ymd, origin, period, yearly)))
+    if len(ymd) == 8:
+        print(str(ymd2tid(int(ymd), int(origin), int(period), yearly)))
+    elif len(ymd) == 7:
+        print(str(ymd2tid(ydoy2ymd(int(ymd)), int(origin), int(period), yearly)))
+    else:
+        print("-1")
 
 if __name__ == "__main__":
    main(sys.argv[1:])
