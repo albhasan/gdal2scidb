c65d35c2b0cbcdb19dfd53fb26d1f26ddaf7c8db
albhasan <albhasan@gmail.com>
2017-10-12 15:19:44 -0300

    call load_parallel.sh with thousands of sdb binary files

---
+++ tests/load_distributed.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+################################################################################
+# SCIDB PARRALEL LOAD
+#-------------------------------------------------------------------------------
+# Call load_parallel.sh with lots of SciDB binary files
+#-------------------------------------------------------------------------------
+# NOTE:
+# - Writes files to a file in order to avoid ERROR: Argument list too long
+################################################################################
+# build a lists of binary chunk files
+export H=12                                 # MODIS TILE H
+export V=09                                 # MODIS TILE V
+export FIRST=40                             # Maximum number of files to list
+export SDB_INSTANCES=35                     # SciDB instances in the whole cluster
+
+# create a list of files to process and feed them to GNU PARALLEL to avoid 
+find /disks/d7/sdb_chunks -type f | grep "mod13q1_h"$H"v"$V"_" | sort | head -n $FIRST > fileslist.txt
+parallel --eta --jobs 1 -n $SDB_INSTANCES --arg-file fileslist.txt bash load_parallel.sh 
+
+exit 0
