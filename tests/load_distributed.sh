325af42b519d72d71d7ae2d1f2e8041689295a1e
albhasan <albhasan@gmail.com>
2017-10-21 22:30:58 -0200

    fixed path updated

--- tests/load_distributed.sh
+++ tests/load_distributed.sh
@@ -14,7 +14,7 @@ export FIRST=40                             # Maximum number of files to list
 export SDB_INSTANCES=35                     # SciDB instances in the whole cluster
 
 # create a list of files to process and feed them to GNU PARALLEL to avoid 
-find /disks/d7/sdb_chunks -type f | grep "mod13q1_h"$H"v"$V"_" | sort | head -n $FIRST > fileslist_h"$H"v"$V".txt
+find /home/scidb/sdb_chunks -type f | grep "mod13q1_h"$H"v"$V"_" | sort | head -n $FIRST > fileslist_h"$H"v"$V".txt
 parallel --eta --jobs 1 -n $SDB_INSTANCES --arg-file fileslist_h"$H"v"$V".txt bash load_parallel.sh 
 
 
