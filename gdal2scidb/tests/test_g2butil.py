af865b1f934000bd1974ddfaafc5361c84cd2f56
albhasan <albhasan@gmail.com>
2017-12-14 17:10:20 -0200

    added prefix to function calls

--- gdal2scidb/tests/test_g2butil.py
+++ gdal2scidb/tests/test_g2butil.py
@@ -27,50 +27,50 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(g2butil.date2ydoy(datetime.date(2000, 2, 29)), 2000060)
         self.assertEqual(g2butil.date2ydoy(datetime.date(2001, 3, 1)), 2001060)
 
-        self.assertEqual(ydoy2ymd(2007365), 20071231)
-        self.assertEqual(ydoy2ymd(2007001), 20070101)
-        self.assertEqual(ydoy2ymd(2000366), 20001231)
-        self.assertEqual(ydoy2ymd(2000060), 20000229)
-        self.assertEqual(ydoy2ymd(2001060), 20010301)
+        self.assertEqual(g2butil.ydoy2ymd(2007365), 20071231)
+        self.assertEqual(g2butil.ydoy2ymd(2007001), 20070101)
+        self.assertEqual(g2butil.ydoy2ymd(2000366), 20001231)
+        self.assertEqual(g2butil.ydoy2ymd(2000060), 20000229)
+        self.assertEqual(g2butil.ydoy2ymd(2001060), 20010301)
 
-        self.assertEqual(ymd2ydoy(20071231), 2007365)
-        self.assertEqual(ymd2ydoy(20070101), 2007001)
-        self.assertEqual(ymd2ydoy(20001231), 2000366)
-        self.assertEqual(ymd2ydoy(20000229), 2000060)
-        self.assertEqual(ymd2ydoy(20010301), 2001060)
+        self.assertEqual(g2butil.ymd2ydoy(20071231), 2007365)
+        self.assertEqual(g2butil.ymd2ydoy(20070101), 2007001)
+        self.assertEqual(g2butil.ymd2ydoy(20001231), 2000366)
+        self.assertEqual(g2butil.ymd2ydoy(20000229), 2000060)
+        self.assertEqual(g2butil.ymd2ydoy(20010301), 2001060)
 
 
 
-        self.assertEqual(ymd2tid(20000101, 20000101, 8, True), 0)
-        self.assertEqual(ymd2tid(20000109, 20000101, 8, True), 1)
-        self.assertEqual(ymd2tid(20000117, 20000101, 8, True), 2)
-        self.assertEqual(ymd2tid(20000102, 20000101, 8, True), 0)
-        self.assertEqual(ymd2tid(20000108, 20000101, 8, True), 0)
-        self.assertEqual(ymd2tid(20000110, 20000101, 8, True), 0)
+        self.assertEqual(g2butil.ymd2tid(20000101, 20000101, 8, True), 0)
+        self.assertEqual(g2butil.ymd2tid(20000109, 20000101, 8, True), 1)
+        self.assertEqual(g2butil.ymd2tid(20000117, 20000101, 8, True), 2)
+        self.assertEqual(g2butil.ymd2tid(20000102, 20000101, 8, True), 0)
+        self.assertEqual(g2butil.ymd2tid(20000108, 20000101, 8, True), 0)
+        self.assertEqual(g2butil.ymd2tid(20000110, 20000101, 8, True), 0)
 
-        self.assertEqual(ymd2tid(20000101, 20000101, 16, True), 0)
-        self.assertEqual(ymd2tid(20000117, 20000101, 16, True), 1)
-        self.assertEqual(ymd2tid(20000202, 20000101, 16, True), 2)
-        self.assertEqual(ymd2tid(20000102, 20000101, 16, True), 0)
-        self.assertEqual(ymd2tid(20000116, 20000101, 16, True), 0)
-        self.assertEqual(ymd2tid(20000118, 20000101, 16, True), 0)
+        self.assertEqual(g2butil.ymd2tid(20000101, 20000101, 16, True), 0)
+        self.assertEqual(g2butil.ymd2tid(20000117, 20000101, 16, True), 1)
+        self.assertEqual(g2butil.ymd2tid(20000202, 20000101, 16, True), 2)
+        self.assertEqual(g2butil.ymd2tid(20000102, 20000101, 16, True), 0)
+        self.assertEqual(g2butil.ymd2tid(20000116, 20000101, 16, True), 0)
+        self.assertEqual(g2butil.ymd2tid(20000118, 20000101, 16, True), 0)
 
-        self.assertEqual(ymd2tid(20000101, 20000101, 16, True), 0)
-        self.assertEqual(ymd2tid(20001218, 20000101, 16, True), 22)
-        self.assertEqual(ymd2tid(20010101, 20000101, 16, True), 23)
-        self.assertEqual(ymd2tid(20011219, 20000101, 16, True), 45)
-        self.assertEqual(ymd2tid(20020101, 20000101, 16, True), 46)
+        self.assertEqual(g2butil.ymd2tid(20000101, 20000101, 16, True), 0)
+        self.assertEqual(g2butil.ymd2tid(20001218, 20000101, 16, True), 22)
+        self.assertEqual(g2butil.ymd2tid(20010101, 20000101, 16, True), 23)
+        self.assertEqual(g2butil.ymd2tid(20011219, 20000101, 16, True), 45)
+        self.assertEqual(g2butil.ymd2tid(20020101, 20000101, 16, True), 46)
 
 
         # tid2ymd and ymd2tid are complementary
         ori = 20000101
         per = 16
         y = True
-        self.assertEqual(tid2ymd(ymd2tid(20000101, ori, per, y), ori, per, y), 20000101)
-        self.assertEqual(tid2ymd(ymd2tid(20001218, ori, per, y), ori, per, y), 20001218)
-        self.assertEqual(tid2ymd(ymd2tid(20010101, ori, per, y), ori, per, y), 20010101)
-        self.assertEqual(tid2ymd(ymd2tid(20011219, ori, per, y), ori, per, y), 20011219)
-        self.assertEqual(tid2ymd(ymd2tid(20020101, ori, per, y), ori, per, y), 20020101)
+        self.assertEqual(g2butil.tid2ymd(g2butil.ymd2tid(20000101, ori, per, y), ori, per, y), 20000101)
+        self.assertEqual(g2butil.tid2ymd(g2butil.ymd2tid(20001218, ori, per, y), ori, per, y), 20001218)
+        self.assertEqual(g2butil.tid2ymd(g2butil.ymd2tid(20010101, ori, per, y), ori, per, y), 20010101)
+        self.assertEqual(g2butil.tid2ymd(g2butil.ymd2tid(20011219, ori, per, y), ori, per, y), 20011219)
+        self.assertEqual(g2butil.tid2ymd(g2butil.ymd2tid(20020101, ori, per, y), ori, per, y), 20020101)
 
 
         inputFiles = '/home/scidb/LANDSAT/landsat8Original/SurfaceReflectance/2013/2013-06-30/LC82260672013181LGN00_cfmask.tif'
