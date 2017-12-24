4a7c7f41cba8cbb0b5643fc18b27afa3ec49b16a
albhasan <albhasan@gmail.com>
2017-12-24 19:37:24 -0200

    fixed bug: File connection name

--- etc/parsesdb.py
+++ etc/parsesdb.py
@@ -30,8 +30,8 @@ def main(argv):
         print("message")
         raise RuntimeError("Error!")
     finally:
-        if not fsdbbin.closed:
-            fsdbbin.close()
+        if not f.closed:
+            f.close()
 
 if __name__ == "__main__":
    main(sys.argv[1:])
