24f2a74af878a35bc4090b6f060f6054ab69bfe2
albhasan <albhasan@gmail.com>
2018-01-05 13:43:20 -0200

    changed landsat level labels

--- gdal2scidb/g2butil.py
+++ gdal2scidb/g2butil.py
@@ -179,7 +179,7 @@ def getFileNameMetadata(filepath):
     filename = os.path.basename(filepath)
     sensorLandsat = {'C':'OLI/TIRS-Combined', 'O':'OLI-only', 'T':'TIRS-only', 'E':'ETM+', 'T':'TM', 'M':'MSS'}
     satelliteLandsat = {'4':'Landsat4','5':'Landsat5','7':'Landsat7', '8':'Landsat8'}
-    processingLevelLandsat = {'L1TP':'Precision-and-Terrain-Correction', 'L1GT':'Systematic-Terrain-Correction', 'L1GS':'Systematic-Correction'}
+    processingLevelLandsat = {'L1TP':'L1TP', 'L1GT':'L1GT', 'L1GS':'L1GS'} # processingLevelLandsat = {'L1TP':'Precision-and-Terrain-Correction', 'L1GT':'Systematic-Terrain-Correction', 'L1GS':'Systematic-Correction'}
     collectionCategoryLandsat = {'RT':'Real Time', 'T1':'Tier 1', 'T2':'Tier 2'}
     sensorModis = {'MOD':'Terra', 'MYD':'Aqua'}
     #
