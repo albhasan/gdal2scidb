5f02d36ffd073a8978959e41534ebc5c6287d156
albhasan <albhasan@gmail.com>
2018-01-31 09:51:45 -0200

    added variable to handle image's name

--- etc/gdal2binImg/load_distributed.sh
+++ etc/gdal2binImg/load_distributed.sh
@@ -8,13 +8,13 @@
 # - Writes files to a file in order to avoid ERROR: Argument list too long
 ################################################################################
 # build a lists of binary chunk files
-export H=12                                 # MODIS TILE H
-export V=08                                 # MODIS TILE V
-export SDB_INSTANCES=35                     # SciDB instances in the whole cluster
+H=12                                 # MODIS TILE H
+V=08                                 # MODIS TILE V
+SDB_INSTANCES=35                     # SciDB instances in the whole cluster
+IMG_PREFIX=MOD                       # Prefix of the image name. i.e. MOD, MYD, LC8
 
 # create a list of files to process and feed them to GNU PARALLEL to avoid
-find -L /home/scidb/sdb_chunks -type f | grep "MOD__13Q1_"$H"_"$V"_" | sort > fileslist_h"$H"v"$V".txt
+find -L /home/scidb/sdb_chunks -type f | grep $IMG_PREFIX"__13Q1_"$H"_"$V"_" | sort > fileslist_h"$H"v"$V".txt
 parallel --eta --jobs 1 -n $SDB_INSTANCES --arg-file fileslist_h"$H"v"$V".txt bash load_parallel.sh
-
 
 exit 0
