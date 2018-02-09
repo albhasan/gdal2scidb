1e9adf5b8e34aaba3fb06fcec77db28706225f99
albhasan <albhasan@gmail.com>
2018-02-09 10:05:20 -0200

    file list prfixed with image type

--- etc/gdal2binImg/load_distributed.sh
+++ etc/gdal2binImg/load_distributed.sh
@@ -14,7 +14,7 @@ SDB_INSTANCES=35                     # SciDB instances in the whole cluster
 IMG_PREFIX=MOD                       # Prefix of the image name. i.e. MOD, MYD, LC8
 
 # create a list of files to process and feed them to GNU PARALLEL to avoid
-find -L /home/scidb/sdb_chunks -type f | grep $IMG_PREFIX"__13Q1_"$H"_"$V"_" | sort > fileslist_h"$H"v"$V".txt
+find -L /home/scidb/sdb_chunks -type f | grep "$IMG_PREFIX"__13Q1_"$H"_"$V"_ | sort > fileslist_"$IMG_PREFIX"_h"$H"v"$V".txt
 parallel --eta --jobs 1 -n $SDB_INSTANCES --arg-file fileslist_h"$H"v"$V".txt bash load_parallel.sh
 
 exit 0
